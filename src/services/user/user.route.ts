5ed2321249635207d4a8a3446b5b76ff9c4916fc
sholehbaktiabadi <sholehbaktiabadi@gmail.com>
2022-09-20 23:04:21 +0700

    refactor(user): change routing rules previledg

--- src/services/user/user.route.ts
+++ src/services/user/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { AppDataSource } from "../../config/database";
-import { authBoth } from "../../middleware/middleware";
+import { authAdmin, authBoth } from "../../middleware/middleware";
 import { UserController } from "./user.controller";
 import { UserRepository } from "./user.repository";
 import { UserService } from "./user.service";
@@ -10,11 +10,11 @@ const userService = new UserService(userRepository)
 const userController = new UserController(userService)
 const router = Router()
 export const userRoutes = [
-    router.post('/user', userController.create),
+    router.post('/user', authBoth, userController.create),
     router.get('/user', authBoth, userController.findAll),
     router.get('/user/:id', authBoth, userController.getDetails),
     router.delete('/user/:id', authBoth, userController.delete),
-    router.patch('/user/:id', authBoth, userController.update),
-    router.put('/user/:id', authBoth, userController.updatePassword)
+    router.patch('/user/:id', authAdmin, userController.update),
+    router.put('/user/:id', authAdmin, userController.updatePassword)
 
 ]
